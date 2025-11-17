95308f53ec7625d7801e3038494c1e7b4daa18b4
Jakob Louis Therkelsen <therkels@msu.edu>
2025-11-17 11:33:35 -0500

    update message

--- routes/commands.py
+++ routes/commands.py
@@ -171,10 +171,7 @@ def slash():
 
         return jsonify({
             "response_type": "ephemeral",
-            "text": f"🎉 You’ve successfully opted in!\n "
-            "Terms of Service: By opting-in, you are agreeing to participating in a term-project for Social Computing, "
-            "CSE 598-012. We only collect responses you provide, which are used with LLMs to generate new channels. If at any point you wish to not "
-            "participate, please use the command '/opt_out'. To review this message, simply type '/opt_in'. Thank you for joining us!"
+            "text": f"Processing opt-in, you should receieve a confirmation DM once you are registered."
         }), 200
 
     if command == "/generate_prompt":
